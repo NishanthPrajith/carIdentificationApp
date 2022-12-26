ba49d78ac113de7622839b03d2dcd96422ae0c93
Nishanth Prajith <44118659+NishanthPrajith@users.noreply.github.com>
2022-12-26 13:59:09 -0500

    Update classifier.dart

--- AppFiles/lib/classifier.dart
+++ AppFiles/lib/classifier.dart
@@ -13,7 +13,6 @@ import 'package:path_provider/path_provider.dart';
 class Test {
 
   String? _imagePrediction;
-  String? _imageTwo;
   
   List? _prediction;
   File? _image;
@@ -35,23 +34,10 @@ class Test {
 
     Model imageModel = await PyTorchMobile.loadModel("assets/models/model_save_actual.pt");
 
-    Model two = await PyTorchMobile.loadModel("assets/models/model_save_actual.pt");
-
-
-    final Uint8List image1 =
-        (await rootBundle.load("assets/001.jpg")).buffer.asUint8List();
-    final tempDir = await getTemporaryDirectory();
-    File image = await File('${tempDir.path}/001.jpg').create();
-    image.writeAsBytesSync(image1);
-
     _imagePrediction = await imageModel!.getImagePrediction(
         img, 300, 300, "assets/labels/labels.csv", mean: meanOurs, std: stdOurs);
 
-    _imageTwo = await two!.getImagePrediction(
-        img, 300, 300, "assets/labels/labels.csv", mean: meanOurs, std: stdOurs);
-
     print(_imagePrediction);
-    _imagePrediction = (_imagePrediction ?? "No") + "," + (_imageTwo ?? "No");
 
     return _imagePrediction ?? "No prediction";
   }
